9173a1657e92b807773d42b23847868f4fea9766
Igor9n <id.igrec@gmail.com>
2019-02-15 08:25:48 +0200

    Added config example for mailer

---
+++ Core/Mailer/config/config.example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igrec
+ * Date: 08.02.19
+ * Time: 9:05
+ */
+
+return [
+    'server' => 'smtp.gmail.com',
+    'port' => 587,
+    'user' => '',
+    'password' => '',
+    'encryption' => 'tls'
+];
